bb5a365280e55011b95efa44162b4f5336c36f43
coderiaser <coderaiser@cloudcmd.io>
2025-12-21 23:40:39 +0200

    feature: stems: add

--- .madrun.js
+++ .madrun.js
@@ -1,4 +1,7 @@
+import {readFile, writeFile} from 'node:fs/promises';
+import {unzipSync} from 'node:zlib';
 import {run} from 'madrun';
+import tryToCatch from 'try-to-catch';
 
 export default {
     'lint': () => 'putout .',
@@ -9,6 +12,18 @@ export default {
     'watch:lint': async () => await run('watcher', await run('lint')),
     'watcher': () => 'nodemon -w test -w lib -w bin --exec',
     'coverage': async () => `c8 ${await run('test')}`,
+    'precoverage': async () => await run('extract'),
     'report': () => 'c8 report --reporter=lcov',
     'wisdom': () => run(['lint', 'coverage']),
+    'extract': async () => {
+        const [error] = await tryToCatch(readFile, './dictionary/stems.json');
+        
+        if (!error)
+            return 'tape';
+        
+        const packed = await readFile('./dictionary/stems.json.gz');
+        await writeFile('./dictionary/stems.json', unzipSync(packed));
+        
+        return 'echo "✅ extract"';
+    },
 };
